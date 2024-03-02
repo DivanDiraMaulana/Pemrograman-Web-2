8731afbb88080f2b03e4b45a697271dfb0fe07c0
Divan Dira Maulana <161866782+DivanDiraMaulana@users.noreply.github.com>
2024-03-02 21:02:40 +0700

    Tugas Pertemuan 1

---
+++ pertemuan 1/lat1/index.php
@@ -0,0 +1,7 @@
+<?php
+print"<pre>";
+print"<center><font size=14 color='red'><b>BIODATA</b></font></center>";
+print"<font size=14 color='blue'>Nama    : <b>Divan Dira Maulana</b></font></br>";
+print"<font size=14 color='blue'>Email   : diradivan5@gmail.com</font></br>";
+print"<font size=14 color='blue'>Jurusan : Tenik Informatika</font></br>";
+print"<font size=14 color='blue'>Hoby    : Futsal</font>";
---
+++ pertemuan 1/lat2/index.php
@@ -0,0 +1,4 @@
+<?php
+print"<pre>";
+print"<center><font size=14 color='red'><b>Hello Saya Divan</b></font></center>";
+print"<font size=14 color='blue'><b><marquee>Kesan awal saya mengerjakan tugas ini ternyata cukup sulit</marquee></b></font></br>";
